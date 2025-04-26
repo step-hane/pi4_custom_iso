dd4b74a4fbfcbc35159d23b0b50ec913b61f29f1
step-hane <49904876+step-hane@users.noreply.github.com>
2025-04-26 20:09:27 +0200

    Update 05-x11.sh

--- stages/02-customize/05-x11.sh
+++ stages/02-customize/05-x11.sh
@@ -1,32 +1,50 @@
 #!/bin/bash -e
+# Installer X11 minimal
 apt-get install -y xserver-xorg xinit xterm
-cat << 'EOMON' > /home/pi/monitor.sh
+
+# Créer le script de monitoring
+cat << 'EOF' > /home/pi/monitor.sh
 #!/bin/bash
+TERM=xterm
 while true; do
   clear
   echo "=== Audio Analyzer Monitor ==="
-  echo "* JACK status:" && systemctl status jackdbus --no-pager
-  echo "* Rust Analyzer (analyzer.service):" && systemctl status analyzer --no-pager
-  echo "* Python TF Server (tfserver.service):" && systemctl status tfserver --no-pager
-  echo "* Network interfaces:" && ip -brief addr
+  echo "* JACK status:"
+  systemctl status jack.service --no-pager
+  echo
+  echo "* Rust Analyzer (analyzer.service):"
+  systemctl status analyzer.service --no-pager
+  echo
+  echo "* Python TF Server (tfserver.service):"
+  systemctl status tfserver.service --no-pager
+  echo
+  echo "* Network interfaces:"
+  ip -brief addr
+  echo
   echo "(Actualisé toutes les 5 s)"
   sleep 5
 done
-EOMON
-chmod +x /home/pi/monitor.sh && chown pi:pi /home/pi/monitor.sh
-cat << 'EXSRV' > /home/pi/.xinitrc
+EOF
+chmod +x /home/${USER_NAME}/monitor.sh
+chown ${USER_NAME}:${USER_NAME} /home/${USER_NAME}/monitor.sh
+
+# Auto-start X11 + monitor
+cat << 'EOF' > /home/${USER_NAME}/.xinitrc
 #!/bin/bash
-xterm -fullscreen -e /home/pi/monitor.sh
-EXSRV
-chown pi:pi /home/pi/.xinitrc
-cat << 'ESYSD' > /etc/systemd/system/autostart-x.service
+# Lancer le script de monitor dans un xterm plein écran
+xterm -fullscreen -e /home/${USER_NAME}/monitor.sh
+EOF
+chown ${USER_NAME}:${USER_NAME} /home/${USER_NAME}/.xinitrc
+
+# Créer service systemd pour démarrer X au login auto
+cat << 'EOF' > /etc/systemd/system/autostart-x.service
 [Unit]
 Description=Auto X11 /home/pi/.xinitrc
 After=graphical.target
 Wants=graphical.target
 
 [Service]
-User=pi
+User=${USER_NAME}
 Environment=DISPLAY=:0
 ExecStart=/usr/bin/startx
 StandardOutput=syslog
@@ -34,5 +52,7 @@ StandardError=syslog
 
 [Install]
 WantedBy=graphical.target
-ESYSD
+EOF
+
+# Activer le service
 systemctl enable autostart-x.service
